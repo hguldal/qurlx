0d1b38c5f3691349523139f2d15529412820dc33
hguldal@gmail.com <hguldal@gmail.com>
2015-02-12 11:53:06 +0200

    N/A

--- scripts/u2p/Default.js
+++ scripts/u2p/Default.js
@@ -4,15 +4,27 @@ $(document).ready(function () {
     function (json) {
         var tr;
         for (var i = 0; i < json.length; i++) {
-            tr = $('<tr/>');
+            tr = $('<tr data-kisaKod="' + json[i].kisaKod + '"/>');
+            tr.append('<td><input type="checkbox"/></td>');
 
-            tr.append("<td>" + json[i].ogeID + "</td>");
-            
-            tr.append("<td>" + json[i].ogeTuru + "</td>");
-            tr.append("<td>" + json[i].ogeAdi + "</td>");
-            tr.append("<td>" + jQuery.timeago(json[i].olusturmaTarihi) + "</td>");
-            tr.append("<td>" + location.href + json[i].kisaKod + "</td>");
+            if (json[i].ogeTuru == 1) {
+                tr.append('<td><span class="glyphicon glyphicon-folder-close" aria-hidden="true"></span><a href="' + location.href + json[i].kisaKod + '"> ' + json[i].ogeAdi + '</a></td>');
+            }
+            else if (json[i].ogeTuru == 2) {
+                tr.append('<td><span class="glyphicon glyphicon-link" aria-hidden="true"></span><a href="' + location.href + json[i].kisaKod + '"> ' + json[i].ogeAdi + '</a></td>');
+            }
+
+            else if (json[i].ogeTuru == 3) {
+                tr.append('<td><span class="glyphicon glyphicon-file" aria-hidden="true"></span><a href="' + location.href + json[i].kisaKod + '"> ' + json[i].ogeAdi + '</a></td>');
+            }
+
+            var tarih = new Date(parseInt(json[i].olusturmaTarihi.substr(6)));
+            tr.append("<td>" + jQuery.timeago(tarih) + "</td>");
+            tr.append('<td><a href="' + location.href + json[i].kisaKod + '">' + location.href + json[i].kisaKod + '</a></td>');
             $('#tableKlasor').append(tr);
         }
     });
+
+    $('#cmbErisimTuru').selectize();
 });
+
