319e76254284fb8b09e1eb708c76a68681d7afb9
hguldal@gmail.com <hguldal@gmail.com>
2015-02-15 23:34:58 +0200

    N/A

--- scripts/u2p/Default.js
+++ scripts/u2p/Default.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
-   
+
     $("#btnUrlEkle").click(function () {
 
-        var url = $('#url').val();
+        var url =prompt("URL'ni buraya yapıştır");
 
-        if (url == '') {
+        if (url == '' || url==null) {
             HataMesaji('Url alanını boş bırakamazsın');
             $('#url').focus();
         }
@@ -16,11 +16,9 @@ $(document).ready(function () {
                 dataType: 'json',
                 success: function (msg) {
                     var tr = $('<tr/>');
-                    
-                    tr.append('<td><span class="glyphicon glyphicon-link" aria-hidden="true"></span><a href="' + msg.url + '"> ' + msg.url + '</a></td><td><a href="' +  location.href + msg.kisaKod   + '">' + location.href + msg.kisaKod +'</td><td></td>');
+
+                    tr.append('<td><h5><span class="glyphicon glyphicon-link" aria-hidden="true"></span><a href="' + msg.url + '"> ' + msg.url + '</a></h5></td><td><h5><a href="' + location.href + msg.kisaKod + '">' + location.href + msg.kisaKod + '</h5></td><td><a href="#" class="btn btn-xs btn-info"><span class="glyphicon glyphicon-cog"></span></a></td>');
                     $('#lstUrl > tbody').prepend(tr);
-                    
-                    $('#url').val('');
 
                 },
                 error: function (msg) {
@@ -31,6 +29,28 @@ $(document).ready(function () {
 
     });
 
+    $('#btnKategoriEkle').click(function(){
+        var kategoriAdi = prompt("Kategoriye bir ad ver");
 
+        if (kategoriAdi!='')
+        {
+            $.ajax({
+                type: "POST",
+                url: '/ajax/KategoriEkle' + '?KategoriAdi=' + kategoriAdi,
+               
+                success: function (kategoriID) {
+
+                    var liste = $('#listeKategori');
+
+                    liste.append('<a href="' + location.href + 'Kategori?KategoriID='  + kategoriID + '" class="list-group-item"><span class="glyphicon glyphicon-tag"></span> ' + kategoriAdi +'<span class="badge">0</span></a>');
+  
+                    
+                },
+                error: function (msg) {
+                    HataMesaji("Beklenmeyen Bir Hata Meydana Geldi");
+                }
+            });
+        }
+    });
 });
 
