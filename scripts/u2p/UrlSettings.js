e520dd659152f99d6c6afca194f8d191e0ef986a
hguldal <hguldal@gmail.com>
2015-02-27 23:56:48 +0200

    N/A

--- scripts/u2p/UrlSettings.js
+++ scripts/u2p/UrlSettings.js
@@ -5,3 +5,26 @@ $('#cmbKategoriSec').selectize({
                 
             }
 });
+
+
+
+$('#txtKisaKod').editable(
+{
+pk: function () {
+return $("#txtGrupWebAdresi").attr("data-GrupID")
+},
+name: 'GrupURL',
+url: '/api/Grup/GrupBilgileriGuncelle',
+display: function (value) {
+$(this).text(value + '');
+},
+success: function (msg) {
+if (msg == 'grupadialinmis') {
+HataMesaji("Group name you have entered already registered");
+return false;
+}
+else {
+MesajKutusu("OK", "Group settings was saved successfuly");
+}
+}
+});
