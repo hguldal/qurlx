2e8025d624310323507641f1ae40b7f7b6d2432c
hguldal <hguldal@gmail.com>
2015-03-01 00:16:18 +0200

    N/A

--- scripts/u2p/UrlSettings.js
+++ scripts/u2p/UrlSettings.js
@@ -21,21 +21,21 @@ $('#datetimepicker2').datetimepicker();
 
 $('.urlSetting').editable(
 {
-pk: function () {
-return $("#txtGrupWebAdresi").attr("data-GrupID")
-},
-name: 'GrupURL',
-url: '/api/Grup/GrupBilgileriGuncelle',
-display: function (value) {
-$(this).text(value + '');
-},
-success: function (msg) {
-if (msg == 'grupadialinmis') {
-HataMesaji("Group name you have entered already registered");
-return false;
-}
-else {
-MesajKutusu("OK", "Group settings was saved successfuly");
-}
-}
+    pk: function () {
+    return $("#txtGrupWebAdresi").attr("data-GrupID")
+    },
+    name: 'GrupURL',
+    url: '/api/Grup/GrupBilgileriGuncelle',
+    display: function (value) {
+    $(this).text(value + '');
+    },
+    success: function (msg) {
+    if (msg == 'grupadialinmis') {
+    HataMesaji("Group name you have entered already registered");
+    return false;
+    }
+    else {
+    MesajKutusu("OK", "Group settings was saved successfuly");
+    }
+    }
 });
