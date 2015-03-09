bf7e9c2a81d28fd19d7835fb4c55fc12f61652c2
hguldal <hguldal@gmail.com>
2015-03-09 10:03:43 +0200

    N/A

--- App_Code/Ziyaret.cs
+++ App_Code/Ziyaret.cs
@@ -26,9 +26,11 @@ public class Ziyaretci
     public string country_code;
     public string country_name;
     public string region_code;
+    public string region_name;
     public string city;
     public string zip_code;
     public string time_zone;
     public string latitude;
     public string longitude;
+   
 }
