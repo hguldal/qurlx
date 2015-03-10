4146d4f51fdac266aa96783c5fd764f4ab421b14
hguldal <hguldal@gmail.com>
2015-03-10 17:06:12 +0200

    N/A

--- scripts/u2p/ByCountry.js
+++ scripts/u2p/ByCountry.js
@@ -1,47 +1,63 @@
+$(document).ready(function () {
 
-        var gData = '{ ';
+    /*
+    var gData = '{ ';
 
-        gData += '"cols": [' +
-                                '{"id":"","label":"Country","pattern":"","type":"string"},' +
-                                '{"id":"","label":"Visitor","pattern":"","type":"number"}' +
-                                '],' +
+    gData += '"cols": [' +
+    '{"id":"","label":"Country","pattern":"","type":"string"},' +
+    '{"id":"","label":"Visitor","pattern":"","type":"number"}' +
+    '],' +
 
-                                '"rows": [';
+    '"rows": [';
 
-        var r= $.ajax({
-            type: "POST",
-            url: '/ajax/stats/UlkeyeGore?kisaKod=' + '1w0i',
-            dataType: 'json',
-            success: function (data) {
+    var r = $.ajax({
+    type: "POST",
+    url: '/ajax/stats/UlkeyeGore?kisaKod=' + '1w0i',
+    dataType: 'json',
+    success: function (data) {
 
-                for (var i = 0; i < data.length; i++) {
-                    gData += '{"c":[{"v":"' + data[i].country_name + '","f":null},{"v":' + data[i].Visitor + ',"f":null}]},';
-                }
-                gData = gData.substring(0, gData.length - 1);
+    for (var i = 0; i < data.length; i++) {
+    gData += '{"c":[{"v":"' + data[i].country_name + '","f":null},{"v":' + data[i].Visitor + ',"f":null}]},';
+    }
+    gData = gData.substring(0, gData.length - 1);
 
-                gData += ']' +
-                            '}';
+    gData += ']' +
+    '}';
 
-            },
-            error: function (msg) {
-                HataMesaji("Unexpected Error !");
-            }
+    },
+    error: function (msg) {
+    HataMesaji("Unexpected Error !");
+    }
 
+    });
+
+    */
+    
+        var randomScalingFactor = function () { return Math.round(Math.random() * 100) };
+        var barChartData = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+
+                        {
+                            fillColor: "rgba(151,187,205,0.5)",
+                            strokeColor: "rgba(151,187,205,0.8)",
+                            highlightFill: "rgba(151,187,205,0.75)",
+                            highlightStroke: "rgba(151,187,205,1)",
+                            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+                        }
+                        ]
+                            };
+
+
+        var ctx = $("#myChart").get(0).getContext("2d");
+
+         var myBar = new Chart(ctx).Bar(barChartData, {
+            responsive: true
         });
 
-    google.load("visualization", "1", { packages: ["geochart"] });
-    google.setOnLoadCallback(haritaCiz);
-
-    function haritaCiz() {
-
-       
-        var data = new google.visualization.DataTable(gData);
 
 
-        var options = {};
+});
 
-        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
 
-        chart.draw(data, options);
-    }
 
