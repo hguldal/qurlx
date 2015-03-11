697817644684481326f511c241517a1165c2cf71
hguldal <hguldal@gmail.com>
2015-03-11 20:09:31 +0200

    N/A

--- scripts/u2p/ByCountry.js
+++ scripts/u2p/ByCountry.js
@@ -1,54 +1,34 @@
 $(function () {
-    
 
-     var secenekler = {
-            chart: {
-                renderTo: 'container',
-                type: 'column'
 
-            },
-            series: []
-        };
+    var secenekler = {
+        chart: {
+            renderTo: 'grafikDiv',
+            type: 'column'
 
-        var grafik = new Highcharts.Chart(secenekler);
+        },
+        series: []
+    };
 
-    $('#tarihAraligi').daterangepicker(
-  {
-      ranges: {
-          'Today': [moment(), moment()],
-          'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-          'Last 7 Days': [moment().subtract('days', 6), moment()],
-          'Last 30 Days': [moment().subtract('days', 29), moment()],
-          'This Month': [moment().startOf('month'), moment().endOf('month')],
-          'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
-      },
-      startDate: moment().subtract('days', 29),
-      endDate: moment()
-  },
-
-   
-  function (start, end) {
-      $('#tarihAraligi').html('<span class="glyphicon glyphicon-calendar"></span> ' + start.format('MMMM,DD YYYY') + '-' + end.format('MMMM,DD YYYY') + ' <b class="caret"></b>');
-
-      $('#tarihAraligi').attr('data-baslangicTarihi', start.format('MM-DD-YYYY'));
-      $('#tarihAraligi').attr('data-bitisTarihi', end.format('MM-DD-YYYY'));
-
-      UlkelereGore_Grafik();
-
-  });
+    var grafik = new Highcharts.Chart(secenekler);
 
     function UlkelereGore_Grafik() {
 
-        var baslangicTarihi = $('#tarihAraligi').attr('data-baslangicTarihi');
-        var bitisTarihi = $('#tarihAraligi').attr('data-bitisTarihi');
+        var baYil = $('#tarihAraligi').attr('data-baYil');
+        var baAy = $('#tarihAraligi').attr('data-baAy');
+        var baGun = $('#tarihAraligi').attr('data-baGun');
+        var biYil = $('#tarihAraligi').attr('data-biYil');
+        var biAy = $('#tarihAraligi').attr('data-biAy');
+        var biGun = $('#tarihAraligi').attr('data-biGun');
+
+
         var kisaKod = $('#txtKisaKod').attr('data-Kisakod');
 
-      
-
-        var url = '/ajax/stats/UlkeyeGore?kisaKod=' + kisaKod + '&baslangicTarihi=' + baslangicTarihi + '&bitisTarihi=' + bitisTarihi;
-
+        var url = '/ajax/stats/UlkeyeGore?kisaKod=' + kisaKod + '&baYil=' + baYil + '&baAy=' + baAy + '&baGun=' + baGun + '&biYil=' + biYil + '&biAy=' + biAy + '&biGun=' + biGun;
+        
         grafik.destroy();
-       
+        secenekler.series = [];
+
         $.getJSON(url, function (yanit) {
 
             for (var i = 0; i < yanit.length; i++) {
@@ -58,7 +38,40 @@ $(function () {
 
             grafik = new Highcharts.Chart(secenekler);
         });
+
     }
+
+
+    $('#tarihAraligi').daterangepicker(
+          {
+              ranges: {
+                  'Today': [moment(), moment()],
+                  'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+                  'Last 7 Days': [moment().subtract('days', 6), moment()],
+                  'Last 30 Days': [moment().subtract('days', 29), moment()],
+                  'This Month': [moment().startOf('month'), moment().endOf('month')],
+                  'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+              },
+              startDate: moment().subtract('days', 29),
+              endDate: moment()
+          },
+
+
+          function (start, end) {
+              $('#tarihAraligi').html('<span class="glyphicon glyphicon-calendar"></span> ' + start.format('MM/DD/YYYY') + '-' + end.format('MM/DD/YYYY') + ' <b class="caret"></b>');
+
+              $('#tarihAraligi').attr('data-baYil', start.format('YYYY'));
+              $('#tarihAraligi').attr('data-baAy', start.format('MM'));
+              $('#tarihAraligi').attr('data-baGun', start.format('DD'));
+              $('#tarihAraligi').attr('data-biYil', end.format('YYYY'));
+              $('#tarihAraligi').attr('data-biAy', end.format('MM'));
+              $('#tarihAraligi').attr('data-biGun', end.format('DD'));
+
+              UlkelereGore_Grafik();
+
+          });
+
+
 
 
 });
