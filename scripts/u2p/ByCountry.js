aeb0829b29c4f1e3ad9652e9882e49dd0f17ad2a
hguldal <hguldal@gmail.com>
2015-03-11 14:38:24 +0200

    N/A

--- scripts/u2p/ByCountry.js
+++ scripts/u2p/ByCountry.js
@@ -1,27 +1,41 @@
 $(function () {
     var bitis = new Date();
-    
 
-    
+
+
     $('#tarihAraligi').daterangepicker(
   {
       ranges: {
-         'Today': [moment(), moment()],
-         'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-         'Last 7 Days': [moment().subtract('days', 6), moment()],
-         'Last 30 Days': [moment().subtract('days', 29), moment()],
-         'This Month': [moment().startOf('month'), moment().endOf('month')],
-         'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+          'Today': [moment(), moment()],
+          'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+          'Last 7 Days': [moment().subtract('days', 6), moment()],
+          'Last 30 Days': [moment().subtract('days', 29), moment()],
+          'This Month': [moment().startOf('month'), moment().endOf('month')],
+          'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
       },
       startDate: moment().subtract('days', 29),
       endDate: moment()
-    } ,
+  },
 
-  
-  function(start, end) {
-        $('#tarihAraligi').html(start.format('MM/DD/YYYY') + ' - ' + end.format('MM/DD/YYYY'));
-    }
-);
+
+  function (start, end) {
+      $('#tarihAraligi').html('<span class="glyphicon glyphicon-calendar"></span> ' + start.format('MMMM-DD-YYYY') + ':' + end.format('MMMM-DD-YYYY') + ' <b class="caret"></b>');
+      $.ajax({
+          type: "POST",
+
+          url: '/ajax/deneme' + '?tarih=' + start.format('MMMM-DD-YYYY'),
+
+          success: function (msg) {
+
+              alert(msg);
+          },
+          error: function (msg) {
+              HataMesaji("Unexpected Error !");
+          }
+      });
+
+
+  });
 
     $("#grafikCiz").click(function () {
 
