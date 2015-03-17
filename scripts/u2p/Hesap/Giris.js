f7bf9e16df5585767cbff9a51be365c5baf94a2f
hguldal <hguldal@gmail.com>
2015-03-17 17:05:22 +0200

    N/A

--- scripts/u2p/Hesap/Giris.js
+++ scripts/u2p/Hesap/Giris.js
@@ -2,20 +2,20 @@ $("#btnLogin").click(function () {
 
     var ePosta = $('#txtEposta').val();
     var parola = $('#txtParola').val();
+    if (ePosta && parola) {
+        $.ajax({
+            url: '/ajax/Hesap/OturumAc.cshtml?txtEposta=' + ePosta + '&txtParola=' + parola,
+            type: 'POST',
+            success: function (msg) {
 
-    $.ajax({
-        url: '/ajax/Hesap/OturumAc.cshtml?txtEposta=' + ePosta + '&txtParola=' + parola,
-        type: 'POST',
-        success: function (msg) {
-            
                 location.href = "/";
-        
-        },
-        error: function (msg) {
 
-            HataMesaji("Login Failed !");
+            },
+            error: function (msg) {
 
-        }
-    });
+                HataMesaji("Login Failed !");
 
+            }
+        });
+    }
 });
