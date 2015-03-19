07d3d025547fb119677d4767c90a0f996ee13ba1
hguldal <hguldal@gmail.com>
2015-03-19 10:19:14 +0200

    N/A

--- App_Code/GoogleSafeBrowsing.cs
+++ App_Code/GoogleSafeBrowsing.cs
@@ -6,26 +6,17 @@ using System.IO;
 
 public class GoogleSafeBrowsing
 {
-    public static bool AdresKontrol(string adres)
+    public static int AdresKontrol(string adres)
     {
 
         
          var googleSafeLookupAdres="https://sb-ssl.google.com/safebrowsing/api/lookup?client=ourlex&key=AIzaSyDYF2551G79-XieSHaIXmzfclylL1bD-jk&appver=1.5.2&pver=3.1&url=" + HttpUtility.UrlEncode(adres);
-          String strResult;
-          WebResponse objResponse;
+          
+       
           WebRequest objRequest = HttpWebRequest.Create(googleSafeLookupAdres);
-         
-          objResponse = objRequest.GetResponse();
-         
-          using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
-          {
-            strResult = sr.ReadToEnd();
-            sr.Close();
-          }
-          if (strResult.Contains("malware") || strResult.Contains("unwanted") || strResult.Contains("phishing"))
-
-          return true;
-          else
-          return false;
-            }
+          HttpWebResponse objResponse= (HttpWebResponse) objRequest.GetResponse();
+          
+         return Convert.ToInt32(objResponse.StatusCode);
+                
+        } 
 }
