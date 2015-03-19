61eacd486021bce65dcec3ffb427a7fb4f82e1c0
hguldal <hguldal@gmail.com>
2015-03-19 11:24:14 +0200

    N/A

--- App_Code/GoogleSafeBrowsing.cs
+++ App_Code/GoogleSafeBrowsing.cs
@@ -6,20 +6,20 @@ using System.IO;
 
 public class GoogleSafeBrowsing
 {
-    public static int AdresKontrol(string adres) 
+    public static bool AdresGuvenilirMi(string adres) 
     {
         try
         {
              var googleSafeLookupAdres="https://sb-ssl.google.com/safebrowsing/api/lookup?client=ourlex&key=AIzaSyDYF2551G79-XieSHaIXmzfclylL1bD-jk&appver=1.5.2&pver=3.1&url=" + HttpUtility.UrlEncode(adres);
              WebRequest objRequest = HttpWebRequest.Create(googleSafeLookupAdres);
              HttpWebResponse objResponse= (HttpWebResponse) objRequest.GetResponse();
-             return Convert.ToInt32(objResponse.StatusCode);
-                //durum kodu=200 ise URL güvenli değil
+             return (Convert.ToInt32(objResponse.StatusCode)!=200);
+                
          }
 
          catch
          {
-             return 204;
+             return true;
          }
       } 
 }
